c9f5a60e925c7657300ac1fd6f2c821232cd92f6
Cauch-BS <cachybs@snu.ac.kr>
2026-02-05 13:45:48 +0900

    chore: update from nix-shell to nix develop

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "pixi env (FHS wrapped)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        fhs = pkgs.buildFHSEnv {
+          name = "pixi-env";
+
+          targetPkgs = pkgs: [
+            pkgs.pixi
+            pkgs.git
+            pkgs.cacert
+          ];
+
+          runScript = "bash";
+        };
+      in
+      {
+        devShell = fhs.env;
+      }
+    );
+}
