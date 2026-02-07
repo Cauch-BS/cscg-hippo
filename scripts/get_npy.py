601499a0d847e1a74dd3d4d49118f9c193aec64e
Seiok Kim <bboyseiok@deepest.ai>
2026-02-07 22:07:59 +0900

    feat: added create_npy.py

--- scripts/get_npy.py
+++ scripts/get_npy.py
@@ -1,15 +1,21 @@
 import numpy as np
-import umap
 
-dataset: dict[str, np.ndarray] = np.load(
+dataset = np.load(
         file = "./dataset.npz",
         mmap_mode = "r",
         allow_pickle = False,  
 )
-spks_big_array: np.ndarray     = dataset["spks"]
+spks_big_array: np.ndarray     = dataset["spks_big_array"]
+day_ind_array: np.ndarray = dataset["day_ind_array"]
 
 np.save(
     file = "spk_data",
     arr = spks_big_array,
     allow_pickle= False,
+)
+
+np.save(
+    file = "day_ind_array",
+    arr = day_ind_array,
+    allow_pickle = False,
 )
